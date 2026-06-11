3e0e43be45290191eca00f4bdcde343f2d985984
ellen-sousa <ellencsousa@outlook.com.br>
2026-06-10 22:16:05 -0300

    edit data nasc

--- lib/dados/api/servico_api_usuario.dart
+++ lib/dados/api/servico_api_usuario.dart
@@ -93,31 +93,33 @@ class ServicoApiUsuario {
   }
 
   Future<String?> atualizarPerfil({
-    required String cpf,
-    required String nome,
-    required String emailRecuperacao,
-    required String tipoDiabetes,
-  }) async {
-    final resposta = await http.put(
-      Uri.parse(
-        '${ApiConfig.baseUrl}/usuarios/perfil/$cpf',
-      ),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode({
-        'nome': nome,
-        'emailRecuperacao': emailRecuperacao,
-        'tipoDiabetes': tipoDiabetes,
-      }),
-    );
+  required String cpf,
+  required String nome,
+  required String emailRecuperacao,
+  required String tipoDiabetes,
+  required DateTime dataNascimento,
+}) async {
+  final resposta = await http.put(
+    Uri.parse(
+      '${ApiConfig.baseUrl}/usuarios/perfil/$cpf',
+    ),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode({
+      'nome': nome,
+      'emailRecuperacao': emailRecuperacao,
+      'tipoDiabetes': tipoDiabetes,
+      'dataNascimento': dataNascimento.toIso8601String(),
+    }),
+  );
 
-    final dados = jsonDecode(resposta.body);
+  final dados = jsonDecode(resposta.body);
 
-    if (resposta.statusCode == 200) {
-      return null;
-    }
-
-    return dados['mensagem'] ?? 'Erro ao atualizar perfil.';
+  if (resposta.statusCode == 200) {
+    return null;
   }
+
+  return dados['mensagem'] ?? 'Erro ao atualizar perfil.';
+}
 }
